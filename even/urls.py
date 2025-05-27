a2a771cda7f2cafd515d71b66a082c1eb46a7c6c
mathber <matheus.bertoldo04@gmail.com>
2025-05-26 22:32:49 -0300

    Criado o componente de Navbar

--- even/urls.py
+++ even/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for even project.
 
@@ -15,8 +16,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('evenapp.urls')),
     path('admin/', admin.site.urls),
 ]
--- evenapp/views.py
+++ evenapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    template = loader.get_template('events\home.html')
+    return HttpResponse(template.render())
