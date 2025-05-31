3a64d9308e3aaea4874b0f199bc7028653641f03
mathber <matheus.bertoldo04@gmail.com>
2025-05-31 13:52:19 -0300

    Finalizando detalhes da home

--- even/settings.py
+++ even/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,12 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
---
+++ evenapp/templatetags/custom_filters.py
@@ -0,0 +1,41 @@
+from django import template
+
+register = template.Library()
+
+dias_semana = {
+    'Monday': 'Segunda',
+    'Tuesday': 'Terça',
+    'Wednesday': 'Quarta',
+    'Thursday': 'Quinta',
+    'Friday': 'Sexta',
+    'Saturday': 'Sábado',
+    'Sunday': 'Domingo'
+}
+
+meses = {
+    'Jan': 'Jan',
+    'Feb': 'Fev',
+    'Mar': 'Mar',
+    'Apr': 'Abr',
+    'May': 'Mai',
+    'Jun': 'Jun',
+    'Jul': 'Jul',
+    'Aug': 'Ago',
+    'Sep': 'Set',
+    'Oct': 'Out',
+    'Nov': 'Nov',
+    'Dec': 'Dez'
+}
+
+@register.filter
+def format_date(value):
+    if not value:
+        return ''
+    
+    dia_semana = dias_semana[value.strftime('%A')]
+    dia = value.strftime('%d')
+    mes = meses[value.strftime('%b')]
+    ano = value.strftime('%Y')
+    hora = value.strftime('%H:%M')
+
+    return f'{dia_semana}, {dia} de {mes} de {ano} às {hora}'
